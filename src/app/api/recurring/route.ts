35dad40846ff31996a4bfe221f43355971d33bce
vincent <vincent.nguyennf@gmail.com>
2025-01-03 15:21:14 -0700

    Prisma client generated

---
+++ src/app/api/recurring/route.ts
@@ -0,0 +1,10 @@
+export async function GET() {
+
+}
+
+/**
+ * Creates a new recurring food request
+ */
+export async function POST(request: Request) {
+    const  = await request.json();
+}
---
+++ src/app/db.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+const PRISMA = new PrismaClient();
+
+export default PRISMA;
