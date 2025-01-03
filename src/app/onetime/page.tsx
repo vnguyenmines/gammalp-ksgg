c978aab0814cced1e85489103f9d05013fb6b953
vincent <vincent.nguyennf@gmail.com>
2025-01-03 14:40:47 -0700

    Simple required frontend added

---
+++ src/app/onetime/page.tsx
@@ -0,0 +1,12 @@
+// One time food request form page
+
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <>
+            <Link href={"/"}>Back home</Link>
+            <div>This page is for the one-time food request form</div>
+        </>
+    );
+}
---
+++ src/app/recurring/page.tsx
@@ -0,0 +1,13 @@
+// Recurring food request form page
+
+import Link from "next/link";
+
+
+export default function Page() {
+    return (
+        <>
+            <Link href={"/"}>Back home</Link>
+            <div>This page is for the recurring food request form</div>
+        </>
+    );
+}
