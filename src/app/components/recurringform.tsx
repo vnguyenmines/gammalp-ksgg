a02a851207a05e68c06786572112156a730acc9f
vincent <vincent.nguyennf@gmail.com>
2025-01-04 14:54:00 -0700

    Added react-hot-toast

--- src/app/components/recurringform.tsx
+++ src/app/components/recurringform.tsx
@@ -1,7 +1,9 @@
 import { IRecurringForm, RecurringRequest } from "@/src/types";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
+import { useEffect } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import toast, { Toaster } from "react-hot-toast"
 
 export default function RecurringForm({ defaultValues }: { defaultValues: IRecurringForm }) {
 
@@ -33,45 +35,54 @@ export default function RecurringForm({ defaultValues }: { defaultValues: IRecur
         mutation.mutate(NEW_RECURRING_REQUEST);
     };
 
+        useEffect(() => {
+            toast.dismiss();
+            if (mutation.isPending) { toast.loading("Saving changes"); }
+            if (mutation.isSuccess) { toast.success("Saved preferences"); }
+        }, [mutation.isPending, mutation.isSuccess]);
+
     return (
-        <form onSubmit={handleSubmit(formSubmit)}>
-            <table className="border-2">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Lunch</th>
-                        <th>Dinner</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>Monday</th>
-                        <td><input type="checkbox" defaultChecked={false} {...register("monday_lunch")} /></td>
-                        <td><input type="checkbox" defaultChecked={false} {...register("monday_dinner")} /></td>
-                    </tr>
-                    <tr>
-                        <th>Tuesday</th>
-                        <td><input type="checkbox" defaultChecked={false} {...register("tuesday_lunch")} /></td>
-                        <td><input type="checkbox" defaultChecked={false} {...register("tuesday_dinner")} /></td>
-                    </tr>
-                    <tr>
-                        <th>Wednesday</th>
-                        <td><input type="checkbox" defaultChecked={false} {...register("wednesday_lunch")} /></td>
-                        <td><input type="checkbox" defaultChecked={false} {...register("wednesday_dinner")} /></td>
-                    </tr>
-                    <tr>
-                        <th>Thursday</th>
-                        <td><input type="checkbox" defaultChecked={false} {...register("thursday_lunch")} /></td>
-                        <td><input type="checkbox" defaultChecked={false} {...register("thursday_dinner")} /></td>
-                    </tr>
-                    <tr>
-                        <th>Friday</th>
-                        <td><input type="checkbox" defaultChecked={false} {...register("friday_lunch")} /></td>
-                        <td><input type="checkbox" defaultChecked={false} {...register("friday_dinner")} /></td>
-                    </tr>
-                </tbody>
-            </table>
-            <button type="submit">Save changes</button>
-        </form>
+        <>
+            <Toaster position="bottom-center" />
+            <form onSubmit={handleSubmit(formSubmit)}>
+                <table className="border-2">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Lunch</th>
+                            <th>Dinner</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Monday</th>
+                            <td><input type="checkbox" defaultChecked={false} {...register("monday_lunch")} /></td>
+                            <td><input type="checkbox" defaultChecked={false} {...register("monday_dinner")} /></td>
+                        </tr>
+                        <tr>
+                            <th>Tuesday</th>
+                            <td><input type="checkbox" defaultChecked={false} {...register("tuesday_lunch")} /></td>
+                            <td><input type="checkbox" defaultChecked={false} {...register("tuesday_dinner")} /></td>
+                        </tr>
+                        <tr>
+                            <th>Wednesday</th>
+                            <td><input type="checkbox" defaultChecked={false} {...register("wednesday_lunch")} /></td>
+                            <td><input type="checkbox" defaultChecked={false} {...register("wednesday_dinner")} /></td>
+                        </tr>
+                        <tr>
+                            <th>Thursday</th>
+                            <td><input type="checkbox" defaultChecked={false} {...register("thursday_lunch")} /></td>
+                            <td><input type="checkbox" defaultChecked={false} {...register("thursday_dinner")} /></td>
+                        </tr>
+                        <tr>
+                            <th>Friday</th>
+                            <td><input type="checkbox" defaultChecked={false} {...register("friday_lunch")} /></td>
+                            <td><input type="checkbox" defaultChecked={false} {...register("friday_dinner")} /></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <button type="submit">Save changes</button>
+            </form>
+        </>
     );
 }
