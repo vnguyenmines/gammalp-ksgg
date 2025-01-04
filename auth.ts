ef334791a5f442b60760796f876e6f2809b1ebfd
vincent <vincent.nguyennf@gmail.com>
2025-01-04 01:19:09 -0700

    Added user creation whenever a new user signs in

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import PRISMA from "@/prisma"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [Google({
@@ -13,4 +14,48 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             }
         }
     })],
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (profile && profile.email && profile.name) {
+                // Ensure the user is created locally in the database
+                await PRISMA.user.findUniqueOrThrow({
+                    where: {
+                        email: profile.email
+                    }
+                }).catch(async (err) => {
+                    if (err.code == "P2025" && profile.email && profile.name) {
+                        await PRISMA.user.create({
+                            data: {
+                                email: profile.email,
+                                name: profile.name,
+                                notes: "sample note"
+                            }
+                        });
+                        console.log(`Created new user ${profile.email}: ${profile.name}`);
+                    }
+                // Check whether the name matches the one in the database
+                }).then(async (user) => {
+                    if (user && user.name != profile.name && profile.name && profile.email) {
+                        await PRISMA.user.update({
+                            data: {
+                                name: profile.name
+                            },
+                            where: {
+                                email: profile.email
+                            }
+                        });
+                    }
+                    else if (user) { return; }
+                    else {
+                        throw Error(`Finding user ${profile.email} broke through Prisma error handling. User value: ${JSON.stringify(user)}`);
+                    }
+                });
+
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
+    }
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,25 +8,27 @@ export default async function Home() {
     <main>
       <h1 className="font-bold">Food Request Form</h1>
       {(session != null && session.user != undefined) ?
-        <>
-          <div>Logged in as {session.user.name}</div>
-          <form action={async () => {
-            "use server";
-            await signOut();
-          }}>
-              <button type="submit">Sign Out</button>
-          </form>
+        <div className="flex flex-col">
+          <div className="flex flex-row gap-2">
+            <span>Logged in as {session.user.name}</span>
+            <form className="mb-4" action={async () => {
+              "use server";
+              await signOut();
+            }}>
+                <button type="submit" className="font-semibold text-blue-600 border-b-2 border-blue-600 pb-0 text-sm">Sign Out</button>
+            </form>
+          </div>
           <div>
             <Link href={"/recurring"} className="block">Recurring Request</Link>
             <Link href={"/onetime"} className="block">One-Time Request</Link>
           </div>
-        </>
+        </div>
         :
         <form action={async () => {
           "use server";
           await signIn("google", { redirectTo: "/" });
         }}>
-          <button type="submit">Sign In</button>
+          <button type="submit" className="font-semibold text-blue-600 border-b-2 border-blue-600 pb-0">Sign In</button>
         </form>}
     </main>
   );
