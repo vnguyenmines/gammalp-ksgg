56fe46a12dcd62b11bbfdab6d6d028e62ee19b5e
vincent <vincent.nguyennf@gmail.com>
2025-01-04 01:29:56 -0700

    Reorganized page elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,15 @@ export default async function Home() {
 
   return (
     <main>
-      <h1 className="font-bold">Food Request Form</h1>
       {(session != null && session.user != undefined) ?
         <div className="flex flex-col">
           <div className="flex flex-row gap-2">
-            <span>Logged in as {session.user.name}</span>
             <form className="mb-4" action={async () => {
               "use server";
               await signOut();
             }}>
                 <button type="submit" className="font-semibold text-blue-600 border-b-2 border-blue-600 pb-0 text-sm">Sign Out</button>
             </form>
-          </div>
-          <div>
-            <Link href={"/recurring"} className="block">Recurring Request</Link>
-            <Link href={"/onetime"} className="block">One-Time Request</Link>
           </div>
         </div>
         :
@@ -30,6 +24,14 @@ export default async function Home() {
         }}>
           <button type="submit" className="font-semibold text-blue-600 border-b-2 border-blue-600 pb-0">Sign In</button>
         </form>}
+
+        <h1 className="font-bold">Food Request Form</h1>
+      {(session != null && session.user != undefined) && 
+          <div>
+            <Link href={"/recurring"} className="block">Recurring Request</Link>
+            <Link href={"/onetime"} className="block">One-Time Request</Link>
+          </div>
+        }
     </main>
   );
 }
