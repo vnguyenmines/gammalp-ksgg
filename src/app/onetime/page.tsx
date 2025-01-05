ebd34d06f457e72d306db103b0555756a6b0d98c
vincent <vincent.nguyennf@gmail.com>
2025-01-04 17:30:30 -0700

    One time form submission implemented

--- src/app/onetime/page.tsx
+++ src/app/onetime/page.tsx
@@ -2,13 +2,17 @@
 "use client"
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { Dispatch, SetStateAction, useState } from "react";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { Calendar as CalendarIcon, Check, ChevronsUpDown } from "lucide-react";
 import { format } from "date-fns"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, } from "@/components/ui/command"
+import { useMutation } from "@tanstack/react-query";
+import { meal, OneTimeRequest } from "@/src/types";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 const frameworks = [
     {
@@ -23,16 +27,39 @@ const frameworks = [
 
 export default function Page() {
     const [date, setDate] = useState<Date | undefined>(new Date(Date.now()));
-    const [meal, setMeal] = useState("");
+    const [meal, setMeal] = useState<meal>();
+    const { mutate } = useMutation({
+        mutationKey: ["post_one_time_request"],
+        mutationFn: async (formInfo: OneTimeRequest) => {
+            console.log(formInfo);
+            const { data, status } = await axios.post("/api/onetime", formInfo);
+            if (status != 200) { toast.error(data.message); }
+            else { toast.success("Request successfully submitted"); }
+        }
+    });
+    useEffect(() => console.log(meal), [meal]);
+
+    function submitOneTimeForm(event: React.MouseEvent<HTMLElement>) {
+        event.preventDefault();
+        if (date == undefined || meal == undefined) { toast.error("Please fill out all fields"); }
+        else {
+            console.log("submitted");
+            mutate({
+                date: date,
+                meal: meal,
+            });
+        }
+    }
 
     return (
         <>
+            <Toaster position="top-center" />
             <Link href={"/"}>👈 Back home</Link>
             <h1>One-time request form</h1>
             <div className="flex flex-col gap-4">
                 <SelectDate date={date} setDate={setDate} />
                 <SelectMeal value={meal} setValue={setMeal} />
-                <Button className="w-min">Submit</Button>
+                <Button className="w-min" onClick={submitOneTimeForm}>Submit</Button>
             </div>
         </>
     );
@@ -65,7 +92,7 @@ function SelectDate({ date, setDate }: { date: Date | undefined, setDate: Dispat
     );
 }
 
-function SelectMeal({ value, setValue }: { value: string, setValue: Dispatch<SetStateAction<string>> }) {
+function SelectMeal({ value, setValue }: { value: meal | undefined, setValue: Dispatch<SetStateAction<meal | undefined>> }) {
     const [open, setOpen] = useState(false);
 
     return (
